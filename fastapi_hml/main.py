7992ab7876e202dc37deea0a9e179729aae739d0
gabrielsrochaa <gabriel.srocha97@gmail.com>
2023-08-17 19:18:51 -0300

    Feat: Adicionando o uso das models

--- fastapi_hml/main.py
+++ fastapi_hml/main.py
@@ -1,4 +1,12 @@
 from fastapi import FastAPI
+from typing import List, Dict, Any
+from models.models import ModeloDoItem
+
+todo: List[Dict[str, Any]] = [
+    {"id":1, "titulo":"Fazer live", "descricao":"Fazer live no canal do Edu"},
+    {"id":2, "titulo":"Ligar Streaming", "status":"A Fazer"},
+    {"id":3, "titulo":"Pentear o cabelo", "status":"A Fazer"}
+]
 
 app = FastAPI()
 
@@ -8,3 +16,10 @@ def hello_world():
     Root view, retorna {"hello": "world"}
     """
     return {"hello":"world"}
+
+@app.get("/a-fazer", response_model=List[ModeloDoItem])
+def listar_a_fazer():
+    """
+    View que retorna lista de itens a fazer
+    """
+    return todo
